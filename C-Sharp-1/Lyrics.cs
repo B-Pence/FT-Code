5529532f6d248a482dcd0e73a884466831118c5f
B-Pence <73327227+B-Pence@users.noreply.github.com>
2022-10-24 15:12:19 -0500

    did first part of unit 1 for cs-1

---
+++ C-Sharp-1/Lyrics.cs
@@ -0,0 +1,13 @@
+using static System.Console;
+class Lyrics
+{
+   static void Main()
+   {
+     //Orange colored sky, by nat king cole
+    // Write your main here
+    WriteLine("When i was walking along, minding my buisness");
+    WriteLine("When out of an orange colored sky");
+    WriteLine("(Flash,bam, alakazam)");
+    WriteLine("Wonderful you came by");
+   }
+}
